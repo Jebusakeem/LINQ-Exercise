567c899804d77e0f82fbaa436d2353e9bd05c0ba
Briun Greene <harmonickarma789@gmail.com>
2026-03-29 13:09:35 -0400

    Created sort method with LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LINQ
+﻿using System.Linq;
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var games = new List<string>()
+                { "Halo", "Call of Duty", "Need for Speed", "Red Dead Redemption", "Grand Theft Auto" };
+
+            var gamesDescending = games.OrderByDescending(x => x.Length).ToList();
+            foreach (var game in gamesDescending)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
